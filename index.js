cfed8fa8de3fc8ac34863017e5111652e9127c4a
inherited-architect-4472 <elaine.onb@gmail.com>
2018-11-07 11:16:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 function theBeatlesPlay(musc, instr){
   var arr = [];
   for(var i = 0; i < musc.length; i++){
-    var str = musc[i] + "plays the"
+    var str = musc[i] + "plays " + 
     
   }
 }
