adbede5c889827c854cc2fac742f226fba16061e
inherited-architect-4472 <elaine.onb@gmail.com>
2018-11-07 11:24:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,11 @@ function theBeatlesPlay(musc, instr){
    return arr;
  } 
  
- + Create a function `theBeatlesPlay`, which accepts two parameters- an array of
+ function theBeatlesPlay(){
+   
+ }
+ 
+ , which accepts two parameters- an array of
 musicians and an array of instruments. The body of the function should create an
 empty array stored in a variable. The function should also contain a for loop
 which loops over the array of musicians. You'll want to be careful about what
@@ -32,3 +36,11 @@ array of new strings.
 + Create a function `johnLennonFacts`. This function will accept one argument, an
 array of facts about John Lennon (note that it might not be exactly the
 following facts):
+
++ Create a function `iLoveTheBeatles` which accepts a number as a parameter. The
+body of the function should create a variable that stores an empty array. Then,
+implement a do-while loop inside the function that adds `"I love the Beatles!"`
+to the empty array. Then the loop should increment the number passed in as a
+parameter. The condition of the loop should check to see that the parameter
+number is less than `15`. The function should return the array with the strings
+`"I love the Beatles!"`.
