93322f47af5cc43476ac39d464fc14b3dec66df4
inherited-architect-4472 <elaine.onb@gmail.com>
2018-11-07 11:30:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,10 +18,17 @@ function theBeatlesPlay(musc, instr){
  } 
 
 
-+ Create a function `johnLennonFacts`. This function will accept one argument, an
-array of facts about John Lennon (note that it might not be exactly the
-following facts):
+ function johnLennonFacts(facts){
+   var i = 0;
+   while( i < facts.length){
+     
+     
+   }
+ }
 
+Use a while loop to loop over the facts array and add `"!!!"` to the end of
+every fact. The function should return an array of strings with exclamation
+points.
 + Create a function `iLoveTheBeatles` which accepts a number as a parameter. The
 body of the function should create a variable that stores an empty array. Then,
 implement a do-while loop inside the function that adds `"I love the Beatles!"`
